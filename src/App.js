7444a9efe52276c0bb7114493753094b944bf0a9
Noel Kujur <lovenoelkujur@gmail.com>
2024-03-20 03:29:06 +0530

    initial project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [bgColor, setBgColor] = useState("black");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"
+        style={{backgroundColor: bgColor}}
+      >
+      
+      <div className='btn-container'>
+        <div className='btn-holder'>
+        <button
+            style={{backgroundColor:"white"}}
+            onClick={() => setBgColor("white")}
+          >White</button>
+
+          <button
+            style={{backgroundColor:"red"}}
+            onClick={() => setBgColor("red")}
+          >Red</button>
+
+          <button
+            style={{backgroundColor:"blue"}}
+            onClick={() => setBgColor("blue")}
+          >Blue</button>
+
+          <button
+            style={{backgroundColor:"green"}}
+            onClick={() => setBgColor("green")}
+          >Green</button>
+
+          <button
+            style={{backgroundColor:"yellow"}}
+            onClick={() => setBgColor("yellow")}
+          >Yellow</button>
+
+          <button
+            style={{backgroundColor:"pink"}}
+            onClick={() => setBgColor("pink")}
+          >Pink</button>
+
+          <button
+            style={{backgroundColor:"orange"}}
+            onClick={() => setBgColor("orange")}
+          >Orange</button>
+
+          <button
+            style={{backgroundColor:"purple"}}
+            onClick={() => setBgColor("purple")}
+          >Purple</button>
+
+          <button
+            style={{backgroundColor:"magenta"}}
+            onClick={() => setBgColor("magenta")}
+          >Magenta</button>
+
+          <button
+            style={{backgroundColor:"black", color: "white"}}
+            onClick={() => setBgColor("black")}
+          >Black</button>
+        </div>
+      </div>
     </div>
   );
 }
